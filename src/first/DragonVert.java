f777162b6c748524ccbc08047467cdf55daffc42
wolfy <wolfy@wolfy-PC>
2019-09-23 17:45:12 +0200

    update lvl up des mobs

--- src/first/DragonVert.java
+++ src/first/DragonVert.java
@@ -7,7 +7,7 @@ public class DragonVert extends Monster{
 		hp=30;
 		mp =12;
 		int baseStats =5;
-		int baseDice =5;
+		int baseDice =8;
 		this.lvl = lvl;
 		xp = lvl * 20;
 		vit = baseStats;
@@ -16,17 +16,43 @@ public class DragonVert extends Monster{
 		adr = baseStats;
 		luck = baseStats;
 		gold = baseStats;
-		for(int i = 0;i<lvl;i++) {
-			gold = gold+ dice.diceThrow(baseDice);
-			hp = hp + dice.diceThrow(15);
-			mp = mp + dice.diceThrow(10);
-			vit = vit+ dice.diceThrow(baseDice);
-			intel = intel+ dice.diceThrow(baseDice);
-			str = str+ dice.diceThrow(baseDice);
-			adr = adr+ dice.diceThrow(baseDice);
-			luck = luck+ dice.diceThrow(baseDice);
-			
+//		for(int i = 0;i<lvl;i++) {
+//			gold = gold+ dice.diceThrow(baseDice);
+//			hp = hp + dice.diceThrow(15);
+//			mp = mp + dice.diceThrow(10);
+//			vit = vit+ dice.diceThrow(baseDice);
+//			intel = intel+ dice.diceThrow(baseDice);
+//			str = str+ dice.diceThrow(baseDice);
+//			adr = adr+ dice.diceThrow(baseDice);
+//			luck = luck+ dice.diceThrow(baseDice);
+//			
+//		}
+		for(int i = 0;i<lvl*baseDice;i++) {
+			int v = dice.diceThrow(5);
+			gold = gold+v+3;//dice.diceThrow(baseDice)
+			switch(v){
+			case 1:
+				vit = vit++; // dice.diceThrow(baseDice)
+				break;
+			case 2:
+				intel = intel++;//+ dice.diceThrow(baseDice)
+				break;
+			case 3:
+				str = str++;//dice.diceThrow(baseDice)
+				break;
+			case 4:
+				adr = adr++;//dice.diceThrow(baseDice)
+
+				break;
+			case 5:
+				luck = luck++;// dice.diceThrow(baseDice)
+				break;}
 		}
+		for(int i = 0;i<lvl ;i++) {
+			hp = hp + dice.diceThrow(baseDice);
+			mp = mp + dice.diceThrow(baseDice);
+		}
+
 		
 		cou = 8+ dice.diceThrow(7);
 		cha= 8+ dice.diceThrow(5);
